77d6c1f6467537b8e92ecdb31890fdcfb36607b2
Prasannawagle <waglewriting@gmail.com>
2024-11-22 14:57:15 -0800

    this

--- client/src/app/destination/page.js
+++ client/src/app/destination/page.js
@@ -1,5 +1,5 @@
 "use client";
-import BasicDateRangePicker from "@/components/dateRangePicker/page";
+import { DatePickerWithRange } from "@/components/datePicker/page";
 import { Button } from "@/components/ui/button";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid"; // Import search icon from Heroicons
 import React from "react";
@@ -7,19 +7,26 @@ import React from "react";
 const Destination = () => {
   return (
     <div className="h-24 bg-gray-100 sticky top-0 z-50 flex justify-center shadow-md">
-      <div className="flex flex-row justify-between items-center w-full max-w-6xl px-4 space-x-6">
-        <div className="relative">
+    <div className="flex flex-row justify-between items-center w-full max-w-6xl px-4 space-x-6">
+    
+        <div className="relative w-1/3">
           <input
             placeholder="Search for Destination"
-            className="focus:outline-none bg-gray-200 hover:bg-gray-300 pl-10 pr-4 py-2 w-full rounded-md h-14 "
+            className="focus:outline-none bg-gray-200 hover:bg-gray-300 pl-10 pr-4 py-2 w-full rounded-md"
           />
           <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-700" />
         </div>
-
-        <BasicDateRangePicker />
-        <Button className="bg-blue-500 rounded-none hover:bg-gray-700">
-          Find a Hotel
-        </Button>
+        <div className="flex">
+           <DatePickerWithRange className="flex-1" />
+           <Button className="ml-4">
+              Room and Guest
+           </Button>
+        </div>
+        <div>
+            <Button className="bg-blue-500 rounded-none hover:bg-blue-400 hover:scale-105">
+                Find a Hotel
+            </Button>
+        </div>
       </div>
     </div>
   );
