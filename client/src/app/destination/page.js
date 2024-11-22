32985b3112d41521e657dcdf39403c5104706d95
Prasannawagle <waglewriting@gmail.com>
2024-11-22 15:58:57 -0800

    change

--- client/src/app/destination/page.js
+++ client/src/app/destination/page.js
@@ -7,12 +7,13 @@ import React from "react";
 const Destination = () => {
   return (
     <div className="h-24 bg-gray-100 sticky top-0 z-50 flex justify-center shadow-md">
-    <div className="flex flex-row justify-between items-center w-full max-w-6xl px-4 space-x-6">
+    <div className="flex flex-row justify-between items-center w-full max-w-1xl px-4 space-x-6">
     
         <div className="relative w-1/3">
           <input
             placeholder="Search for Destination"
-            className="focus:outline-none bg-gray-200 hover:bg-gray-300 pl-10 pr-4 py-2 w-full rounded-md"
+            className="focus:outline-none bg-gray-300 hover:bg-gray-300 pl-10 pr-4 py-2 w-full rounded-md placeholder-gray-600
+            border border-black border-opacity-10 shadow-offset-black p-4 bg-black rounded-md"
           />
           <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-700" />
         </div>
@@ -23,7 +24,7 @@ const Destination = () => {
            </Button>
         </div>
         <div>
-            <Button className="bg-blue-500 rounded-none hover:bg-blue-400 hover:scale-105">
+            <Button className="bg-yellow-300  mr-0 text-black-500 hover:bg-blue-500 hover:text-white hover:scale-105 !rounded">
                 Find a Hotel
             </Button>
         </div>
